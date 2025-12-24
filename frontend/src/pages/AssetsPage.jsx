984c0cb7522f6f5d63df280cf471d93cb693fde7
Samuel Leonetti <samleo312@gmail.com>
2025-12-24 13:49:15 -0500

    frontend structure, basic react url routing created.

---
+++ frontend/src/pages/AssetsPage.jsx
@@ -0,0 +1,13 @@
+export default function AssetsPage() {
+  return (
+    <div className="h-screen flex">
+      <aside className="w-64 border-r p-2">
+        <div className="font-semibold">Assets</div>
+      </aside>
+
+      <main className="flex-1 p-4">
+        <div className="text-lg font-semibold">Details</div>
+      </main>
+    </div>
+  )
+}
